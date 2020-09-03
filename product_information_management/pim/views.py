159bfb025ded256fcdff46e191139e976f084b8f
Sajid Samsad <samsadsajid@gmail.com>
2020-09-03 21:21:44 +0600

    Refactor deletion code for Category feature to remove multiple db calling

--- product_information_management/pim/views.py
+++ product_information_management/pim/views.py
@@ -4,8 +4,7 @@ from rest_framework.response import Response
 from utility.request_validation import is_invalid_create_category_request_body
 from utility.utility import (
     convert_request_body_to_json, generate_invalid_req_body_error_message_response,
-    map_create_category, generate_bad_req_body_error_message_response, object_exists_with_this_category,
-    object_to_be_deleted_exists_with_this_category
+    map_create_category, generate_bad_req_body_error_message_response, object_exists_with_this_category
 )
 from .models import Category
 
@@ -42,10 +41,9 @@ def create_delete(request):
     body = convert_request_body_to_json(request)
     category_name, _ = map_create_category(body)
 
-    if object_to_be_deleted_exists_with_this_category(category_name):
-        category = Category.objects.get(name=category_name)
-        category.isDeleted = 1
-        category.save()
+    category = Category.objects.filter(name=category_name, isDeleted=0)
+    if category.exists():
+        category.update(isDeleted=1)
 
         data = {
             "message": "Category deleted successfully"
